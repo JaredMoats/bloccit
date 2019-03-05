3af912c305e727bf4c140512fb79a776c6ba018a
Jared Moats <JaredMoats@outlook.com>
2019-03-05 14:01:49 -0500

    Checkpoint 5: Relational Error

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,6 +3,9 @@ module.exports = {
   on the Express object*/
   init(app) {
     const staticRoutes = require('../routes/static');
+    const topicRoutes = require("../routes/topics");
+
     app.use(staticRoutes);
+    app.use(topicRoutes);
   }
 }
