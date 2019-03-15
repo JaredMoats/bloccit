aae140e1c3b2f53a6a9f588deee61547e6287c46
Jared Moats <JaredMoats@outlook.com>
2019-03-15 14:52:08 -0400

    Checkpoint 6 The Topic Resource Part 2 Complete

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -9,5 +9,57 @@ module.exports = {
         response.render("topics/index", {topics});
       }
     });
+  },
+  new(request, response, next) {
+    response.render("topics/new");
+  },
+  create(request, response, next) {
+    let newTopic = {
+      title: request.body.title,
+      description: request.body.description
+    };
+    topicQueries.addTopic(newTopic, (error, topic) => {
+      if(error) {
+        response.redirect(500, "/topics/new");
+      } else {
+        response.redirect(303, `/topics/${topic.id}`);
+      }
+    });
+  },
+  show(request, response, next) {
+    topicQueries.getTopic(request.params.id, (error, topic) => {
+      if(error || topic == null) {
+        response.redirect(404, "/");
+      } else {
+        response.render("topics/show", { topic });
+      }
+    });
+  },
+  destroy(request, response, next) {
+    topicQueries.deleteTopic(request.params.id, (error, topic) => {
+      if(error) {
+        response.redirect(500, `/topics/${topic.id}`);
+      } else {
+        response.redirect(303, "/topics");
+      }
+    });
+  },
+  edit(request, response, next) {
+    topicQueries.getTopic(request.params.id, (error, topic) => {
+      if(error || topic == null) {
+        response.redirect(404, "/");
+      } else {
+        response.render("topics/edit", {topic});
+      }
+    });
+  },
+  update(request, response, next) {
+    topicQueries.updateTopic(request.params.id, request.body, (error, topic) => {
+      if(error || topic == null) {
+        response.redirect(404, `/topics/${request.params.id}/edit`);
+      } else {
+        response.redirect(`/topics/${topic.id}`);
+      }
+    });
   }
 };
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -3,12 +3,61 @@ const Topic = require("./models").Topic;
 module.exports = {
   getAllTopics(callback) {
     return Topic.all()
-
     .then(topics => {
       callback(null, topics);
     })
     .catch(error => {
       callback(error);
+    });
+  },
+  addTopic(newTopic, callback) {
+    return Topic.create({
+      title: newTopic.title,
+      description: newTopic.description
+    })
+    .then(topic => {
+      callback(null, topic);
+    })
+    .catch(error => {
+      callback(error);
+    });
+  },
+  getTopic(id, callback) {
+    return Topic.findById(id)
+    .then(topic => {
+      callback(null, topic);
+    })
+    .catch(error => {
+      callback(error);
+    });
+  },
+  deleteTopic(id, callback) {
+    return Topic.destroy({
+      where: {id}
+    })
+    .then(topic => {
+      callback(null, topic);
+    })
+    .catch(error => {
+      callback(error);
+    });
+  },
+  updateTopic(id, updatedTopic, callback) {
+    return Topic.findById(id)
+    .then(topic => {
+      if(!topic) {
+        return callback("Topic not found");
+      }
+
+      topic.update(updatedTopic, {
+        fields: Object.keys(updatedTopic)
+      })
+      .then(() => {
+        callback(null, topic);
+      })
+      .catch(error => {
+        callback(error);
+      });
     });
   }
 };
