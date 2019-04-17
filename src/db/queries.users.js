9869045ce9979054b37240ade1046c6fc5786f02
Jared Moats <JaredMoats@outlook.com>
2019-04-17 13:45:37 -0400

    Assignment Public Profiles Complete

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,6 +1,7 @@
 const User = require("./models").User;
 const Post = require("./models").Post;
 const Comment = require("./models").Comment;
+const Favorite = require("./models").Favorite;
 const bcrypt = require("bcryptjs");
 
 module.exports = {
@@ -41,10 +42,17 @@ module.exports = {
               .then(comments => {
                 // #7
                 result["comments"] = comments;
-                callback(null, result);
-              })
-              .catch(err => {
-                callback(err);
+
+                Favorite.scope({ method: ["lastFiveFor", id] })
+                  .all()
+                  .then(favorites => {
+                    result["favorites"] = favorites;
+
+                    callback(null, result);
+                  })
+                  .catch(err => {
+                    callback(err);
+                  });
               });
           });
       }
